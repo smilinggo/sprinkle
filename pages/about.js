78b2bc42cfa6e61aff6a29d8602d31879c8b897c
Toney Tate <toney@greencompassglobal.com>
2023-03-26 14:39:21 -0400

    Update about.js
    Remove team section from about us

--- pages/about.js
+++ pages/about.js
@@ -88,55 +88,7 @@ const About = () => {
             </dl>
           </div>
         </div>
-      </div>
-      <div className="bg-yellow-500">
-        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-          <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
-            Our Team
-          </h2>
-          <div className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            <div className="group relative">
-              <div className="overflow-hidden rounded-lg h-80">
-                <img
-                  src="https://images.unsplash.com/photo-1579697410491-6d5a6f5da6b5?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-                  alt=""
-                  className="w-full h-full object-center object-cover group-hover:opacity-75 transition-opacity duration-500"
-                />
-              </div>
-              <h3 className="mt-4 text-lg font-medium text-gray-700">
-                John Doe
-              </h3>
-              <p className="text-gray-500">Head Baker</p>
-            </div>
-            <div className="group relative">
-              <div className="overflow-hidden rounded-lg h-80">
-                <img
-                  src="https://images.unsplash.com/photo-1605312893034-e4f8a59c4a80?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-                  alt=""
-                  className="w-full h-full object-center object-cover group-hover:opacity-75 transition-opacity duration-500"
-                />
-              </div>
-              <h3 className="mt-4 text-lg font-medium text-gray-700">
-                Jane Smith
-              </h3>
-              <p className="text-gray-500">Pastry Chef</p>
-            </div>
-            <div className="group relative">
-              <div className="overflow-hidden rounded-lg h-80">
-                <img
-                  src="https://images.unsplash.com/photo-1564564296102-2c86bbf02a67?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-                  alt=""
-                  className="w-full h-full object-center object-cover group-hover:opacity-75 transition-opacity duration-500"
-                />
-              </div>
-              <h3 className="mt-4 text-lg font-medium text-gray-700">
-                Mike Johnson
-              </h3>
-              <p className="text-gray-500">Sous Chef</p>
-            </div>
-          </div>
         </div>
-      </div>
     </>
   );
 };
